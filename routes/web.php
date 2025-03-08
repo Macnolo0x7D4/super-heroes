856bc4cb55480ee48bef4d952db5d9b31b774a84
Manuel Díaz <yosoymacnolo@gmail.com>
2025-03-08 00:43:19 -0600

    feat: add gender controller and view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\GenderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/genders', [GenderController::class, 'index']);
